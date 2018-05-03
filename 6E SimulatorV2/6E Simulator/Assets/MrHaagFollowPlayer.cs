ed909afb3f3bf08e7047d60ccf88fc4c48247538
Polybyte <35924486+Polybyte@users.noreply.github.com>
2018-05-03 16:13:18 +0200

    HE WILL FIND YOU.
    HE WILL FOLLOW YOU. HE WILL GIVE YOU A DETENTION (finally spelled right!)

--- 6E SimulatorV2/6E Simulator/Assets/MrHaagFollowPlayer.cs
+++ 6E SimulatorV2/6E Simulator/Assets/MrHaagFollowPlayer.cs
@@ -4,50 +4,22 @@ using UnityEngine;
 
 public class MrHaagFollowPlayer : MonoBehaviour
 {
-
-    public Transform target;
-    public Transform staticTarget;
-    public Transform myTransform;
     public int normalSpeed;
-    public int superSpeed;
-
-    public float timer1;
-    public float timer2;
+    private GameObject FPSController;
     public bool isMad;
 
     void Start()
     {
+        FPSController = GameObject.Find("FPSController");
     }
     void Update()
     {
-        if (!isMad)
-        {
-            timer1 += Time.deltaTime;
-        }
-        else
-        {
-            timer2 += Time.deltaTime;
-        }
+        //transform.LookAt(FPSController.transform);
 
-        if (timer1 >= 10)
-        {
-            isMad = true;
-            timer1 = 0;
-        }
-
-        if (timer2 >= 1)
-        {
-            isMad = false;
-            timer2 = 0;
-        }
-            if (!isMad)
+            if (isMad)
             {
+                //GÅ FKN FRAM
                 transform.Translate(Vector3.back * normalSpeed * Time.deltaTime);
-            }
-            else
-            {
-                transform.Translate(Vector3.back * superSpeed * Time.deltaTime);
-            }
-        
+            } 
     }
 }
