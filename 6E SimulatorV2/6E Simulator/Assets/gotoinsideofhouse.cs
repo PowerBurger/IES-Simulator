7966fcab12c566a843312c2e7092227219c98f46
filip05cool <adinfilip@gmail.com>
2018-05-23 22:19:28 +0200

    The old house
    the old house is currently in development and you will be able to enter the house maybe even some kind of quest ;))

---
+++ 6E SimulatorV2/6E Simulator/Assets/gotoinsideofhouse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+public class gotoinsideofhouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "FistCol")
+        {
+            SceneManager.LoadScene("Inside of the old house"); 
+        }
+    }
+}
