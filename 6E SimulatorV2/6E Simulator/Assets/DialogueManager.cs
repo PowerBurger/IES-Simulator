022fce00dae0d6daaf354bfb568a90f8ee4b8a1c
SpeedFrame <speedframegames@gmail.com>
2018-05-26 15:30:44 +0200

    Dialogue System 1

---
+++ 6E SimulatorV2/6E Simulator/Assets/DialogueManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Queue<string> sentences;
+
+	void Start ()
+    {
+        sentences = new Queue<string>();
+	}
+}
